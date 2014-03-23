b5fd4dd6a639f31e16ae09978888b158685abba3
Miguel Dickson <luis.miguel.dickson@gmail.com>
2014-03-23 18:16:05 -0400

    Finish statics

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+        it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+	  expect(page).to have_title('Home Page')
+    end
+		
+	
+	
+  end
+ 
+   describe "Help page" do
+		it "should have the content 'Help'" do 
+		visit '/static_pages/help'
+		expect(page).to have_content('Help')
+		expect(page).to have_title('Help Page')
+		end
+	end
+
+	describe "About page" do	
+		it "should have the content 'About'" do
+		visit '/static_pages/about'
+		expect(page).to have_content('About')
+		expect(page).to have_title('About Page')
+		end
+	end
+	
+	
+	
+	
+end
